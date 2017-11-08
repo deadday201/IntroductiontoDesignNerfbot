62290cc067b4b0597e8a2944e907662de72f7008
deadday201 <9267746+deadday201@users.noreply.github.com>
2017-11-08 15:35:31 -0600

    Update manual.py

--- manual.py
+++ manual.py
@@ -4,17 +4,22 @@ import pygame
 import time
 import atexit
 # index of motor HAT being called
-derby = Adafruit_MotorHAT(addr=0x60)
-stepper1 = derby.getStepper(200, 1)
-stepper1.setSpeed(30)
+derby = Adafruit_MotorHAT(addr=0x60) #(address) default 0x60
+stepper1 = derby.getStepper(200, 1) #(steps per rev, port)
+stepper1.setSpeed(30) #(rev per minute)
+#TURN OFF
 def turnOffMotors();
-	stepperHAT.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
-	stepperHAT.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
-	stepperHAT.getMotor(3).run(Asafruit_MotorHAT.RELEASE) 
-	stepperHAT.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+	derby.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+	derby.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+	derby.getMotor(3).run(Asafruit_MotorHAT.RELEASE) 
+	derby.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
 atexit.register(turnOffMotors)
+#VARIABLES
+#fastEngaged = True
 interval = 0.1
-stepSpeed = 10
+stepSpeedFast = 10
+stepSpeedSlow = 1
+stepSpeed
 pygame.init()
 pygame.key.set_repeat(1, interval)
 screen = pygame.display.set_mode([400, 400])
@@ -24,13 +29,34 @@ def funcPygameKey(events):
 		if event.type == pygame.QUIT:
 			print ('quit pygame')
 		elif event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_a:
-				print ('key a -turn left')
+			if event.key == pygame.K_RETURN:
+				if stepSpeed == stepSpeedFast:
+					stepSpeed = stepSpeedSlow
+				else:
+					stepSpeed = stepSpeedFast
+				#if fastEngaged:
+				#	fastEngaged = False
+				#else:
+				#	fastEngaged = True
+			elif event.key == pygame.K_a:
+				print('key a - turn ccw @ ' stepSpeed)
 				stepper1.step(stepSpeed, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.DOUBLE)
+				#if fastEngaged:
+				#	print ('key a -turn left fast')
+				#	stepper1.step(stepSpeedFast, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.DOUBLE)
+				#else:
+				#	print ('key a -turn left slow')
+				#	stepper1.step(stepSpeedSlow, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.DOUBLE)
 			elif event.key == pygame.K_d:
-				print ('key d -turn right')
+				#if fastEngaged:
+				#	print ('key d -turn right fast')
+				#	stepper1.step(stepSpeedFast, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.DOUBLE)
+				#else
+				#	print ('key d -turn right slow')
+				#	stepper1.step(stepSpeedSlow, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.DOUBLE)
+				print ('key d - turn cw @ ' stepSpeed)
 				stepper1.step(stepSpeed, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.DOUBLE)
-			else event.key == pygame.k_SPACE:
+			elif event.key == pygame.k_SPACE:
 				print ('key space -fire the guns')
 				#insert trigger motor code here
 try:
